2730f0566c7b5ef22c6ff2ed0d76d5f53825e219
Akash <akasgj007@gmail.com>
2024-02-27 18:35:02 +0530

    Fetch the Genres

---
+++ src/hooks/useGenres.1.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { Genre, FetchGenreResponse } from "./useGenres";
+
+export const useGenres = () => {
+  const [genres, setGenres] = useState<Genre[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    setLoading(true);
+    apiClient
+      .get<FetchGenreResponse>("/games", { signal: controller.signal })
+      .then((res) => {
+        setGenres(res.data.results);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  return { games, error, isLoading };
+};
